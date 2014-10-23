d80dbe5f99f71a9d44bca68984628ef3977853eb
Rob Clarke <robert.b.clarke@gmail.com>
2014-10-23 18:14:19 +0100

    revised tests to use a bogus model as get_absolute_url no longer supported on user model by django 1.7

--- laconicurls/tests.py
+++ laconicurls/tests.py
@@ -1,46 +1,64 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
 from laconicurls.models import Shortcut, laconic_url_for_object
+from django.db import models
+from django.utils.http import urlquote
 
+class NonsenseQuotation(models.Model):
+    """A dummy model that implements get_absolute_url"""
+    quote = models.TextField()
+
+    def __str__(self):
+        return self.quote
+
+    def get_absolute_url(self):
+        return "/stupid-quotations/%s/" % urlquote(self.quote)
 
 class BaseTestCase(TestCase):
 
     def setUp(self):
-        self.user_model = get_user_model()
-        self.user = self.user_model.objects.create_user('test','test@example.com')
+        self.quotation = NonsenseQuotation.objects.create(quote="I wander lonely as a cloud")
+
+    def assertRedirectsWithoutCaringAbout404(self, *args, **kwargs):
+        """A messy workaround to prevent the test client complaining about not being able to find the final url, which we don't actually care about"""
+        try:
+            self.assertRedirects(*args, **kwargs)
+        except AssertionError:
+            self.assertRedirects(*args, target_status_code=404, **kwargs)
+
 
     def tearDown(self):
-        #clean up users
-        self.user_model.objects.all().delete()
+        #clean up db
+        NonsenseQuotation.objects.all().delete()
         Shortcut.objects.all().delete()
 
 class UserLinkTests(BaseTestCase):
 
     def test_simple_shortcut(self):
-        url = laconic_url_for_object(self.user)
-        res = self.client.get(url)
-        self.assertRedirects(res, self.user.get_absolute_url(), status_code=301)
+        url = laconic_url_for_object(self.quotation)
+        res = self.client.get(url, follow=True)
+        res.redirect_chain[0][0]
+        self.assertRedirectsWithoutCaringAbout404(res, self.quotation.get_absolute_url(), status_code=301)
 
     def test_no_duplicates(self):
-        url1 = laconic_url_for_object(self.user)
-        url2 = laconic_url_for_object(self.user)
+        url1 = laconic_url_for_object(self.quotation)
+        url2 = laconic_url_for_object(self.quotation)
         self.assertEquals(url1, url2)
 
     def test_not_found(self):
-        short_url = laconic_url_for_object(self.user)
-        self.user.delete()
+        short_url = laconic_url_for_object(self.quotation)
+        self.quotation.delete()
         res = self.client.get(short_url)
         self.assertEquals(res.status_code, 404)
 
     def test_case_insensitive(self):
-        short_url = laconic_url_for_object(self.user)
+        short_url = laconic_url_for_object(self.quotation)
         for u in [short_url.lower(), short_url.upper()]:
-            self.assertRedirects(self.client.get(u), self.user.get_absolute_url(), status_code=301)
+            self.assertRedirectsWithoutCaringAbout404(self.client.get(u), self.quotation.get_absolute_url(), status_code=301)
 
 class BadLinkTests(BaseTestCase):
 
     def test_no_link_to_shortcut(self):
-        s1 = Shortcut(content_object = self.user)
+        s1 = Shortcut(content_object = self.quotation)
         s1.save()
         self.assertRaises(Shortcut.InvalidLinkObject, laconic_url_for_object, s1)
 
